a24f437b394697cd3431c5db3bb1751b22502e49
Shishirise <155470989+Shishirise@users.noreply.github.com>
2026-03-17 11:13:11 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,20 @@
 
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyARBw3pgnhRbEjxywrKIWTftMsA1KPVf8g",
+  authDomain: "shishirise.firebaseapp.com",
+  projectId: "shishirise",
+  messagingSenderId: "488840494684",
+  appId: "1:488840494684:web:446724e7de437d510f3099"
+});
+
+const messaging = firebase.messaging();
+
+// 🔔 Background notification (when app closed)
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+  });
+});
